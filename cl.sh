73bd27aef9abb2b4e1c5360961d41b40fc0fbbaf
Zack Light <zack.hardtoname@gmail.com>
2021-06-14 10:33:06 -0400

    Clean repository

---
+++ cl.sh
@@ -0,0 +1,26 @@
+shopt -s globstar
+
+declare -A arr
+for f in test/**; do
+  # if a directory, skip
+  [[ -d "$f" ]] && continue
+  lines=0
+  # strip the extension
+  ext="${f##*.}"
+  # convert it to lowercase
+  ext="${ext,,}"
+  # if no dot in the name, extension is "empty"
+  [[ ! $(basename "$f") =~ \. ]] && ext="empty"
+  # count the lines
+  lines=$(wc -l "$f"| cut -d' ' -f1)
+  # if lines equals to 0, skip
+  [[ $lines -eq 0 ]] && continue
+  # append the number of line to the array
+  lines=$(( "${arr[$ext]}"+$lines ))
+  arr[$ext]=$lines 
+done
+
+# loop over the array
+for n in ${!arr[@]}; do
+  echo "files $n: total lines ${arr[$n]}"
+done
--- cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zack/github/googletest_example/lib/googletest/googletest
-# Build directory: /home/zack/github/googletest_example/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
